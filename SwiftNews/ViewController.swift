86f06b268e56ccca06186620770d5116f58b6168
modeverv <modeverv@gmail.com>
2019-04-05 15:54:14 +0900

    Initial Commit

---
+++ SwiftNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftNews
+//
+//  Created by seijiro on 2019/04/05.
+//  Copyright © 2019 norainu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
